adbddd1e293079e14ecebf0c8d8003d0628abbc6
Omer Haroon <omermalik237@gmail.com>
2026-09-18 13:13:50 -0700

    Remove database password from settings

--- chronosfair/settings.py
+++ chronosfair/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'chronosfair',
-        'USER': 'root',
-        'PASSWORD': 'REMOVED_PASSWORD',
+        'USER': 'xxx',
+        'PASSWORD': 'xxx',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
